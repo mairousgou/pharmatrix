82293115f69275428e47304c94d145eb7b793d8b
mairousgou-benedicte <benedictemairousgou@gmail.com>
2025-08-12 05:59:26 -0700

    Gestion de la navigation

---
+++ detaildrugs.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detaildrugs</title>
+    <link rel="stylesheet" href="./assets/css/detaildrugs.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-6.7.2-web/css/all.min.css">
+</head>
+<body>
+   
+  <?php include('includes/nav.php'); ?>
+    <section id="main">
+        <h2> medicaments/paracetamol</h2>
+          <div class="drug">
+            <div class="image">
+              <a href="#">
+                <img src="assets/images/paracetamol.jpg" alt="paracetamol">
+              </a>
+            </div>
+            <div class="texte">
+              <h3>paracetamol</h3>
+              <p>
+                lorem ipsum dolor sit amet consecte iste magnan assumenda nostrum amet cupiditate pariatur
+              </p>
+            </div>
+          </div>
+          <?php include('includes/footer.php'); ?>
+
+      
+           
+      
+</body>
+</html>
